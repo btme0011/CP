b6d15a4d852be6de74a10e4cdef7f04f09680959
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-03-20 17:20:13 +0530

    Update • seive for prime number and prime factorization.cpp

--- • seive for prime number and prime factorization.cpp
+++ • seive for prime number and prime factorization.cpp
@@ -1,5 +1,9 @@
- for(int i=2;i<=sqrt(n);i++){
-        for(int j=i*i;j<n;j+=i){
-                seive[j]=1;
-         }
-}
+vector<ll> seive(100,1);
+	ll n=100;
+	for(int i=2;i*i<=n;i++){
+		if(seive[i]==1){
+			for(int j=i*i;j<=n;j+=i){
+				seive[j]=0;
+			}
+		}
+	}
