98c98b0bf0ab96bc742a21c83123011333e121f9
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-20 23:49:49 +0530

    Update matrix exponentiation.cpp

--- matrix exponentiation.cpp
+++ matrix exponentiation.cpp
@@ -1,8 +1,12 @@
-vector<vector<ll>> mat_mul{
-     for(int i=0;i<n;i++){
-          for(int j=0;i<n;i++){
-                 for(int k=0;k<n;k++){
-                          vec[i][k]+=vec[i][j]*vec[j][k];
+vector<vector<ll>> mat_mul(vector<vector<ll>> vec1,vector<vector<ll>> vec2){
+     ll r1=vec1.size();
+     ll c1=vec1[0].size();
+     ll c2=vec2[0].size();
+     vector<vector<ll>> ans(
+     for(int i=0;i<r1;i++){
+          for(int j=0;i<c2;i++){
+                 for(int k=0;k<c1;k++){
+                          vec[i][j]+=vec[i][k]*vec[k][j];
                   }
            }
      }
@@ -11,8 +15,9 @@ vector<vector<ll>> mat_mul{
 expo(vector<vector<ll>> a,int b){
            vector<vector<ll>> ans={{1,0},{0,1}};
            while(b>0){
-                  if(b%2!=0) {mat_mul(ans,a);b--;}
-                   else {mat_mul(ans,ans);b/=2;}
+                  if(b%2!=0) {ans=mat_mul(ans,a);b--;}
+                   else {a=mat_mul(a,a);b/=2;}
             }
+     return ans;
      }
 
