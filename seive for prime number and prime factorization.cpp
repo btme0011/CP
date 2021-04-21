5eb87818b4866e26133f267a90b8dfcad81a9fc1
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 08:01:05 +0530

    Update seive for prime number and prime factorization.cpp

--- seive for prime number and prime factorization.cpp
+++ seive for prime number and prime factorization.cpp
@@ -22,13 +22,13 @@ template<typename T>
 struct f_seive{
 	ll n;
 	vector<ll> seive;
-	is_p_seive(ll s){
+	f_seive(ll s){
 		n=s;
 		seive=vector<ll>(s,1);
 		make();
 	}
 	void make(){
-		for(int i=2;i<=n;i++){
+		for(int i=2;i*i<=n;i++){
 			if(seive[i]==1){
 				for(int j=i*i;j<=n;j+=i){
 					if(seive[j]==1)
