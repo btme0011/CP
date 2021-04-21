1fb381ef0212a89fbc39d9400b9bfeb9afdd7321
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 07:36:27 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -3,16 +3,16 @@
 // "abcabcd" -> [0,0,0,1,2,3,0]     "aabaaab" -> [0,1,0,1,2,2,3].
 // O(n)
 
-vector<int> prefix_function(string s) {
-    int n = (int)s.length();
-    vector<int> pi(n);
-    for (int i = 1; i < n; i++) {
-        int j = pi[i-1];
+vector<ll> prefix_function(string s) {// pass the string for which you want the pi function
+    int n = (ll)s.length();
+    vector<ll> pi(n);
+    for (ll i = 1; i < n; i++) {
+        ll j = pi[i-1];
         while (j > 0 && s[i] != s[j])
             j = pi[j-1];
         if (s[i] == s[j])
             j++;
         pi[i] = j;
     }
-    return pi;
+    return pi; // vector of ll pi
 }
