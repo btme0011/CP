e785144d489fb6b7a98b697fc5356ad8c4cd1286
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-05-06 17:15:33 +0530

    Update seive for prime number and prime factorization.cpp

--- seive for prime number and prime factorization.cpp
+++ seive for prime number and prime factorization.cpp
@@ -1,16 +1,16 @@
 template<typename T>
 struct is_p_seive{
-	ll n;
-	vector<ll> seive;// get the seive
-	is_p_seive(ll s){// give the size of the seive
+	T n;
+	vector<T> seive;// get the seive
+	is_p_seive(T s){// give the size of the seive
 		n=s;
-		seive=vector<ll>(s,1);
+		seive=vector<T>(s,1);
 		make();
 	}
 	void make(){
-		for(int i=2;i*i<=n;i++){
+		for(T i=2;i*i<=n;i++){
 			if(seive[i]==1){
-				for(int j=i*i;j<=n;j+=i){
+				for(T j=i*i;j<=n;j+=i){
 					seive[j]=0;
 				}
 			}
@@ -20,17 +20,17 @@ struct is_p_seive{
 
 template<typename T>
 struct f_seive{
-	ll n;
-	vector<ll> seive;// get the seive
-	f_seive(ll s){// give the size of the seive 
+	T n;
+	vector<T> seive;// get the seive
+	f_seive(T s){// give the size of the seive 
 		n=s;
-		seive=vector<ll>(s,1);
+		seive=vector<T>(s,1);
 		make();
 	}
 	void make(){
-		for(int i=2;i*i<=n;i++){
+		for(T i=2;i*i<=n;i++){
 			if(seive[i]==1){
-				for(int j=i*i;j<=n;j+=i){
+				for(T j=i*i;j<=n;j+=i){
 					if(seive[j]==1)
 						seive[j]=i;
 				}
