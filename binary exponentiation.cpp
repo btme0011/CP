9f74aa0a9f75c2f89c179030768fd1c7365fd606
Akhil Anand <71667635+btme0011@users.noreply.github.com>
2023-11-28 18:57:16 +0530

    Update binary exponentiation.cpp

--- binary exponentiation.cpp
+++ binary exponentiation.cpp
@@ -1,7 +1,7 @@
  expo(int a,int b){
            int ans=1;
            while(b>0){
-                  if(b%2!=0) ans*=a;b--;
-                   else ans*=ans;b/=2;
+                  if(b%2!=0) {ans*=a;b--;}
+                   else {a*=a;b/=2;}
             }
      }
